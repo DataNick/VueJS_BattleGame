e267197ece82906670c0799eb0bfb1ed54178473
DataNick <DataNick@users.noreply.github.com>
2018-05-03 17:42:55 -0400

    Set up dynamic styles on progress bar

--- app.js
+++ app.js
@@ -1,12 +1,31 @@
 new Vue({
   el: "#app",
   data: {
-    startGame: false
+    startGame: false,
+    battleLog: [],
+    youStyle: {
+      backgroundColor: 'green',
+      margin: 0,
+      color: 'white'
+    },
+    monsterStyle: {
+      backgroundColor: 'green',
+      margin: 0,
+      color: 'white'
+    }
   },
   methods: {
-
-  },
-  computed: {
-
+    setAttack: function(){
+      console.log("attack!");
+    },
+    specialAttack: function(){
+      console.log('special attack');
+    },
+    setHeal: function(){
+      console.log("Heal thyself");
+    },
+    giveUp: function(){
+      console.log("Run Away!");
+    }
   }
 })
